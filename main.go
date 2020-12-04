c01f2d94901c4f5c587063f6f2a09e905d659cd4
Nick Stefan <NickStefan12@gmail.com>
2020-12-04 10:34:49 -0700

    fix command consumer by having both command chan and tick chan

--- main.go
+++ main.go
@@ -1,5 +1,35 @@
 package main
 
+import "time"
+
+func gameLoop(game *Game, actions Actions, cc chan Command, mc chan<- Message) {
+	ticker := time.NewTicker(1 * time.Second)
+	for {
+		select {
+		case c := <-cc:
+			action, argument := c.Parse()
+			switch c.origin {
+			case USER:
+				if actionFunction, ok := actions.user[action]; ok {
+					actionFunction(argument, game, mc)
+				} else {
+					mc <- createMessage("Invalid Command")
+				}
+			case GAME:
+				if actionFunction, ok := actions.game[action]; ok {
+					actionFunction(argument, game, mc)
+				} else {
+					mc <- createMessage("Invalid Command")
+				}
+			default:
+				mc <- createMessage("Invalid Command: Missing Origin")
+			}
+		case <-ticker.C:
+			game.CheckTriggers(cc)
+		}
+	}
+}
+
 func main() {
 	cc := make(chan Command, 1)
 	mc := make(chan Message, 1)
@@ -13,7 +43,8 @@ func main() {
 		npcsByLoc:               make(map[int][]NPC),
 	}
 	actions := createActions()
-	go consumeCommands(actions, cc, game, mc)
+	//go consumeCommands(actions, cc, game, mc)
+	go gameLoop(game, actions, cc, mc)
 	cc <- Command{"/init", GAME}
 
 	createUI(cc, mc)
